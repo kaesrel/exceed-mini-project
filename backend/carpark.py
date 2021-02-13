5785bfc83a1c32f00fd095546bd1dfab2b8b1397
banksinn <sintanawongsupakdee@gmail.com>
2021-02-13 13:24:36 +0700

    update carpark.py

--- backend/carpark.py
+++ backend/carpark.py
@@ -7,5 +7,34 @@ mongo =PyMongo(app)
 
 myCollection = mongo.db.g13
 
+@app.route('/create', methods=['POST'])
+def insert():
+    data = request.json
+    myInsert = {
+        "carpark": data["carpark"], #string Carpark_{x}
+        "status": data["status"], #string {"available","parking","timeout"}
+        "timein": data["timein"],  #string {"12.00"}
+        "duration": data["duration"]  #int {5} minute
+    }
+    myCollection.insert_one(myInsert)
+    return {"result": "Create succesfully"}
+
+@app.route('/replace', methods=['PUT'])
+def replace():
+    data = request.json
+    carpark_name = request.args.get('carpark')
+
+    filt = {"carpark": carpark_name} #158.108.182.15:50003/replace?carpark="Carpark_{x}"
+
+    updated_content = {"$set": {
+        "carpark": data["carpark"],
+        "status": data["status"],
+        "timein": data["timein"],
+        "duration": data["duration"]
+    }}
+
+    myCollection.update_one(filt, updated_content)
+    return {"result": "Replace successfully"}
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='50003', debug=True)
