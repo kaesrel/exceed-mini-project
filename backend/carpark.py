45a88aaad81c0eeb207482f18eabe58d18678f47
Zeedneo <kak.tfk@gmail.com>
2021-02-13 12:23:42 +0700

    test

--- backend/carpark.py
+++ backend/carpark.py
@@ -3,9 +3,10 @@ from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 app.config['MONGO_URI'] = 'mongodb://exceed_user:1q2w3e4r@158.108.182.0:2277/exceed_backend'
-mongo =PyMongo(app)
+mongo = PyMongo(app)
 
 myCollection = mongo.db.g13
 
+print("test")
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='50003', debug=True)
